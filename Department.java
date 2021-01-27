babc638984262510e14e4edeb4bab8d7ca135450
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-27 18:21:09 +0530

    Inheritance

---
+++ Department.java
@@ -0,0 +1,34 @@
+/*import java.util.Scanner;
+
+public class Department extends Collage {   	
+
+	public static void main(String[] args) {
+
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the total time spent in the Department:");
+		float a=scan.nextFloat();
+		float tt = 6;
+		Department d = new Department();
+		d.time(a,tt);
+		
+		scan.close();
+	}
+	}*/
+
+
+import java.util.Scanner;
+
+public class Department extends Collage {   
+	
+	public static void main(String[] args) {
+
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the total time spent in the Department:");
+		float a=scan.nextFloat();
+		float tt = 6;
+		Department d = new Department();
+		d.time(a,tt);
+		
+		scan.close();
+	}
+	}
